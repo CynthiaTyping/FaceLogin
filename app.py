81ecf358eea4c6a8302a6a3186cd539ede9b19a5
Xinjie <xinjiel@usc.edu>
2023-10-27 22:50:30 -0700

    add webcam feature

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for
+import os
+import base64
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.secret_key = 'secret_key_here'  # For flashing messages
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        image_data = request.form.get('imageData')
+
+        # Convert base64 image data to an image file
+        image_data = image_data.split(",")[1]
+        with open(os.path.join(app.config['UPLOAD_FOLDER'], f"{name}.png"), "wb") as f:
+            f.write(base64.decodebytes(image_data.encode()))
+
+        # Now you have the image, integrate the facial recognition logic here
+
+        flash("Login successful!", "success")
+        return redirect(url_for('index'))
+
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
