c42bab7fb9fb226f76fba2b9e0000198bf2833e5
Xinjie <xinjiel@usc.edu>
2023-10-29 02:48:09 -0700

    Add model API

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 import os
 import base64
+from flask import jsonify
+from static.models.model import SiameseModel
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.secret_key = 'secret_key_here'  # For flashing messages
 
+# Path to the true image (you may need to adjust this based on your setup)
+TRUE_IMAGE_PATH = os.path.join(app.config['UPLOAD_FOLDER'], 'true_img.png')
+
+# Instantiate the SiameseModel class
+siamese = SiameseModel()
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 @app.route('/upload', methods=['GET', 'POST'])
 def get_image():
     if request.method == 'POST':
@@ -18,16 +26,53 @@ def get_image():
 
         # Convert base64 image data to an image file
         image_data = image_data.split(",")[1]
-        with open(os.path.join(app.config['UPLOAD_FOLDER'], f"{name}.png"), "wb") as f:
+        uploaded_img_path = os.path.join(app.config['UPLOAD_FOLDER'], f"{name}.png")
+        with open(uploaded_img_path, "wb") as f:
             f.write(base64.decodebytes(image_data.encode()))
 
-        # Now you have the image, integrate the facial recognition logic here
+        # Predict similarity
+        similarity_score = siamese.predict_similarity(TRUE_IMAGE_PATH, uploaded_img_path)
 
-        flash("Login successful!", "success")
-        return redirect(url_for('index'))
+        # Decide on a threshold for similarity score to consider the upload successful
+        threshold = 0.7
+        if similarity_score >= threshold:
+            flash("Login successful!", "success")
+        else:
+            flash("Login failed! Please try again.", "danger")
+
+        return redirect(url_for('home'))
 
     return render_template('index.html')
 
+@app.route('/predict_similarity', methods=['POST'])
+def predict_similarity():
+    # Check if the post request has the files part
+    if 'true_img' not in request.files or 'uploaded_img' not in request.files:
+        return jsonify({"error": "No file part in the request"}), 400
+
+    true_img = request.files['true_img']
+    uploaded_img = request.files['uploaded_img']
+
+    # Check for empty files
+    if true_img.filename == '' or uploaded_img.filename == '':
+        return jsonify({"error": "No selected files"}), 400
+
+    # Create temporary paths for saving uploaded files
+    true_img_path = os.path.join("tmp", true_img.filename)
+    uploaded_img_path = os.path.join("tmp", uploaded_img.filename)
+
+    # Save the files
+    true_img.save(true_img_path)
+    uploaded_img.save(uploaded_img_path)
+
+    # Predict
+    similarity_score = siamese.predict_similarity(true_img_path, uploaded_img_path)
+
+    # Cleanup
+    os.remove(true_img_path)
+    os.remove(uploaded_img_path)
+
+    return jsonify({"similarity_score": similarity_score})
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/models/model.py
@@ -0,0 +1,34 @@
+import cv2
+from keras.models import load_model
+from keras import backend as K
+
+
+class SiameseModel:
+    def __init__(self):
+        self.model = self.load_siamese_model('siamese_nn.h5')
+
+    @staticmethod
+    def euclidean_distance(vectors):
+        vector1, vector2 = vectors
+        sum_square = K.sum(K.square(vector1 - vector2), axis=1, keepdims=True)
+        return K.sqrt(K.maximum(sum_square, K.epsilon()))
+
+    @staticmethod
+    def contrastive_loss(Y_true, D):
+        margin = 1
+        return K.mean(Y_true * K.square(D) + (1 - Y_true) * K.maximum((margin-D),0))
+
+    def load_siamese_model(self, model_path):
+        return load_model(model_path, custom_objects={'contrastive_loss': self.contrastive_loss, 'euclidean_distance': self.euclidean_distance})
+
+    def prepare_image(self, img_path):
+        img = cv2.imread(img_path, 0)
+        img = img.astype('float32')/255
+        img = cv2.resize(img, (92, 112))
+        img = img.reshape(1, img.shape[0], img.shape[1], 1)
+        return img
+
+    def predict_similarity(self, true_img_path, uploaded_img_path):
+        true_img = self.prepare_image(true_img_path)
+        uploaded_img = self.prepare_image(uploaded_img_path)
+        return 1 - self.model.predict([true_img, uploaded_img])[0][0]
